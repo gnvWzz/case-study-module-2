9d149642af85b2bd5a799c725f1d69807966f5d8
gnvWzz <quangvuongdac2111@gmail.com>
2023-04-26 16:29:03 +0700

    completed-project

---
+++ src/RoomPackage/ComparatorRoomAlphabet.java
@@ -0,0 +1,10 @@
+package RoomPackage;
+
+import java.util.Comparator;
+
+public class ComparatorRoomAlphabet implements Comparator<Room> {
+    @Override
+    public int compare(Room r1, Room r2) {
+        return r1.getId().compareTo(r2.getId());
+    }
+}
